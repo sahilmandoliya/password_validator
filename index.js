16c71110188abfe33d906d9a0cc231b9ec6e191b
sahilmandoliya <mandoliyasahil@gmail.com>
2024-01-10 23:58:53 +0530

    validatePassword class

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+class validPassword{
+    constructor(password){
+        this.password=password;
+    }
+    isValid(){
+       return this.password.length>=8 ? true : false;
+    }
 
+    hasUpperCaseCharacter(){
+        return this.password.toLowerCase() != this.password;
+    }
+
+    hasNumber(){
+        return /\d/.test(this.password);
+    }
+};
 
 function validatePassword(password){
     const isValidPassword=new validPassword(password);
