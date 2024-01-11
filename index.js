25f7230b8d05a89f833e96324128263f741ab772
sahilmandoliya <mandoliyasahil@gmail.com>
2024-01-11 23:58:15 +0530

    not common password function and isStrongPassword

--- index.js
+++ index.js
@@ -13,6 +13,28 @@ class ValidPassword{
     hasNumber(){
         return /\d/.test(this.password);
     }
+
+    hasSpecialCharacter(){
+        return /[!@#$%^&*]/.test(this.password);
+    }
+
+    isNotCommonPassword() {
+        const commonPasswords=["password", "123456", "qwerty", "12345678"]
+        return !commonPasswords.includes(this.password.toLowerCase());
+    }
+
+    isStrongPassword() {
+        return (
+            this.isValidLength() &&
+            this.hasUpperCaseCharacter() &&
+            this.hasLowerCaseCharacter() &&
+            this.hasNumber() &&
+            this.hasSpecialCharacters() &&
+            this.hasNoSpaces() &&
+            this.hasNoConsecutiveRepeatedCharacters() &&
+            this.isNotCommonPassword()
+        );
+    }
 };
 
 function validatePassword(password){
